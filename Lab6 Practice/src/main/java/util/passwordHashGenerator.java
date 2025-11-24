8f57ac5af943eab1100be6c964483c76f88f14c4
KhanhTaiTran <tktai2003@gmail.com>
2025-11-24 10:23:33 +0700

    Fix folder name

---
+++ Lab6 Practice/src/main/java/util/passwordHashGenerator.java
@@ -0,0 +1,21 @@
+package util;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class passwordHashGenerator {
+
+    public static void main(String[] args) {
+        String plainPassword = "khanhtai";
+
+        // Generate hash
+        String hashedPassword = BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+
+        System.out.println("Plain Password: " + plainPassword);
+        System.out.println("Hashed Password: " + hashedPassword);
+        System.out.println("\nCopy the hashed password to your INSERT statement");
+
+        // Test verification
+        boolean matches = BCrypt.checkpw(plainPassword, hashedPassword);
+        System.out.println("\nVerification test: " + matches);
+    }
+}
